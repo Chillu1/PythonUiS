ae37f09bfa4a0e1afcdd8998fd832b1ccc8a7b5d
Chillu1 <lukasz20002604@gmail.com>
2021-02-15 16:07:14 +0100

    Fixed typo

--- Oving7/main.py
+++ Oving7/main.py
@@ -3,7 +3,7 @@ from tur import Tur
 tur = Tur("First", 2, 3)
 tur.add_posisjon_koordinater(2,3,2)
 tur.add_posisjon_koordinater(20,5,20)
-tur.add_posisjon_koordinater(18,13,2)
+tur.add_posisjon_koordinater(18,13,22)
 tur.add_posisjon_koordinater(2,3,2)
 
 print(f"Hoydemeter: {tur.hoydemeter()}")
