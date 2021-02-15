4c6f702993add74f18c19526c9b558814bc17531
Chillu1 <lukasz20002604@gmail.com>
2021-02-15 15:26:32 +0100

    Added base scripts for task 7 0 progress/chnages, like stated

---
+++ Oving7/main.py
@@ -0,0 +1,18 @@
+from tur import Tur
+
+tur = Tur("First", 2, 3)
+tur.add_posisjon_koordinater(2,3,2)
+tur.add_posisjon_koordinater(20,5,20)
+tur.add_posisjon_koordinater(18,13,2)
+tur.add_posisjon_koordinater(2,3,2)
+
+print(f"Hoydemeter: {tur.hoydemeter()}")
+print(f"Rundetur: {tur.er_rundtur()}")
+
+tur = Tur("Second", 2, 3)
+tur.add_posisjon_koordinater(5,3,10)
+tur.add_posisjon_koordinater(15,5,18)
+tur.add_posisjon_koordinater(12,15,12)
+
+print(f"Hoydemeter: {tur.hoydemeter()}")
+print(f"Rundetur: {tur.er_rundtur()}")
